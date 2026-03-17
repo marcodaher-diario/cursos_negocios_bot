59bdd6a32d2d66ca7fa7ff42a1c6d7b5559eb388
marcodaher-diario <marco.daher@gmail.com>
2026-03-17 20:36:31 -0300

    Update template_blog.py

--- template_blog.py
+++ template_blog.py
@@ -7,9 +7,9 @@ import re
 CONFIG_VISUAL = {
     "cor_md": "rgb(7, 55, 99)",
     "fonte": "Arial, sans-serif",
-    "tamanho_titulo": "26px",
-    "tamanho_sub": "19px",
-    "tamanho_texto": "17px"
+    "tamanho_titulo": "28px",
+    "tamanho_sub": "26px",
+    "tamanho_texto": "20px"
 }
 
 def formatar_texto_ultra(texto_bruto, titulo_principal):
